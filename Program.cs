94c8dcf1052ca32ac86f2626d0d0a33061e219ff
ionut <ionutcatana1805@gmail.com>
2023-12-31 18:22:06 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    // Creating a struct called Number
+    public struct Number
+    {
+        // Creating a property called "Amount" of data type decimal
+        public decimal Amount { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an object of data type Number and initializing it
+            Number number = new Number();
+
+            // Assigning an amount to the Number object
+            number.Amount = 18.05M;
+
+            // Printing the amount to the console
+            Console.WriteLine("Amount: " + number.Amount);
+
+            Console.ReadLine();
+        }
+    }
+}
